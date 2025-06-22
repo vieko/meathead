4d78a5c018d78545b89ba65768acdcfc9d5956be
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-06-21 19:09:19 -0600

    adds shadcn, starts prototype schemas, outlines project

---
+++ public/specs/prototype.md
@@ -0,0 +1,17 @@
+## Cycle Planning
+
+We are going to build a kanban-style board for planning our cycles:
+
+- The user selects how many weeks the cycle will last from a.
+- The user selects what weight units the cycle will use. The options are pounds
+  or kilograms.
+- The user names the cycle.
+- The user creates days of the week (columns) then select a label or name for
+  each.
+- The user can delete a day.
+- The user cna change the label.
+- Each day (column) contains muscle groups (cards).
+- The user creates groups (cards) for each day (column) then selects an exercise
+  of the given type (muscle group). Only one exercise per card.
+- Once all days have groups and all groups have exercises, the user can create a
+  cycle.
---
+++ src/lib/schemas/prototype.ts
@@ -0,0 +1,72 @@
+import { pgTable, text, uuid, timestamp } from 'drizzle-orm/pg-core'
+
+// === TEMPLATES ===
+export const templates = pgTable('templates', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+// === DAYS OF THE WEEK ===
+export const days = pgTable('days', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+// === MUSCLE GROUPS ===
+export const muscles = pgTable('muscles', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+// === EXERCISES ===
+export const exercises = pgTable('exercises', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+// === CYCLES ===
+export const cycles = pgTable('cycles', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+// === WORKOUT LOGS ===
+export const logs = pgTable('logs', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+// === USERS ===
+export const users = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
